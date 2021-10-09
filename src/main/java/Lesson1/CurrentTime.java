5275e8c5bb4b85afd938244e8e3e9dc7c6256287
ihorm <ihor.moroz.ukr@gmail.com>
2021-10-09 16:26:27 +0300

    Chapter3

--- src/main/java/Lesson1/CurrentTime.java
+++ src/main/java/Lesson1/CurrentTime.java
@@ -1,7 +1,20 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class CurrentTime {
     public static void main(String[] args) {
-
+        long currentTime=System.currentTimeMillis();
+        long seconds, minutes,hours;
+        int GMT;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please enter GMT: ");
+        GMT=input.nextInt();
+        seconds=currentTime/1000;
+        minutes=seconds/60;
+        hours=minutes/60;
+        System.out.print((hours%24+GMT)%24);
+        System.out.print(":"+ minutes%60);
+        System.out.print(":"+ seconds%60);
     }
 }
